e0cede16bbe1e94c047ef4cc1da03018f97e1a4a
Leonte Levinta <llevintza@gmail.com>
2019-04-29 11:50:46 +0300

    added base solution with the web-api project

---
+++ src/LargeFileUploader/Controllers/FileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LargeFileUploader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpPost]
+        [Produces("application/json")]
+        public void Post()
+        {
+        }
+    }
+}
